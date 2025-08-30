4d08abd06b3b8c9a9df7c90e226e48012600743f
SaiNagaAnirudh13 <sainagaanirudh@gmail.com>
2025-08-30 21:24:31 +0530

    Initial commit - RetailTech backend

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
+// app.js
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.get('/api', (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ message: "Hello, Azure!" });
 });
 
-app.listen(port, () => {
-  console.log(`Backend API running on port ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Backend server running on port ${PORT}`);
 });
